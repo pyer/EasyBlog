9beb4f58deffd4de66081b21cf487d386ceb7ce7
pyer <pierre.bazonnard@gmail.com>
2024-11-20 18:51:09 +0100

    Keeping the language selection in a cookie

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,64 @@
-//Header fixed
+// Select lang
+function setCookie(value) {
+  let exp = new Date();
+  // cookie expires in 1 year
+  exp.setTime(exp.getTime() + 31536000000);
+  document.cookie = "lang=" + value + ";expires="+ exp.toUTCString();
+  document.getElementById("lang").value = value;
+}
 
+function show(elements) {
+  for (var i = 0; i < elements.length; i++) {
+    elements[i].style.display = "initial";
+  }
+}
+
+function hide(elements) {
+  for (var i = 0; i < elements.length; i++) {
+    elements[i].style.display = "none";
+  }
+}
+
+function selectEnglish() {
+  console.log("selectEnglish");
+  /*
+  document.getElementById("en").style.display = "initial";
+  document.getElementById("fr").style.display = "none";
+  */
+  hide(document.getElementsByClassName("fr"));
+  show(document.getElementsByClassName("en"));
+  setCookie("en_US");
+}
+
+function selectFrench() {
+  console.log("selectFrench");
+  hide(document.getElementsByClassName("en"));
+  show(document.getElementsByClassName("fr"));
+  setCookie("fr_FR");
+}
+
+document.getElementById("lang").onchange = function(evt) {
+  const lang = evt.target.value;
+  console.log(lang);
+  if (lang === "en_US") {
+    selectEnglish();
+  } else {
+    selectFrench();
+  }
+}
+
+window.onload = function () {
+  const lang = document.cookie;
+  console.log("Cookie : %s", lang);
+  if (lang === "lang=en_US") {
+    selectEnglish();
+  } else {
+    // default lang is french
+    selectFrench();
+  }
+}
+
+//Header fixed
 window.onscroll = function () {
   const docScrollTop = document.documentElement.scrollTop;
 
@@ -34,22 +93,4 @@ const hamBurger = document.querySelector(".hamburger");
 hamBurger.addEventListener("click", function () {
   document.querySelector(".navbar").classList.toggle("show");
 })
-
-/*
-const closeLightbox = document.querySelector(".close-lightbox");
-const lightbox = document.querySelector(".lightbox");
-const lightboxImg = lightbox.querySelector("img");
-
-lightbox.addEventListener("click", function () {
-  if (event.target != lightboxImg) {
-    lightbox.classList.remove("show");
-    lightbox.classList.add("hide");
-  }
-});
-
-closeLightbox.addEventListener("click", function () {
-  lightbox.classList.remove("show");
-  lightbox.classList.add("hide");
-});
-*/
 
